db4270a8777cb48709add5b88e63e67b71dfa8a1
Luke Hinds <lukehinds@gmail.com>
2025-07-24 09:13:50 +0100

    Add URL and SHA

--- Formula/agentup.rb
+++ Formula/agentup.rb
@@ -3,8 +3,8 @@ class Agentup < Formula
 
   desc "AgentUp is a tool for building and deploying AI agents"
   homepage "https://github.com/RedDotRocket/AgentUp"
-  url "https://files.pythonhosted.org/packages/agentup-0.3.0.tar.gz"
-  sha256 "de918cfe4b8df435ea6ed3bf5e49bda86efc53a3e833fdffee8bef63aba173b5"
+  url "https://files.pythonhosted.org/packages/a2/2b/7179dfe6041849ea6d155f06f5c1bbcbae78849114ad5e3464921bb11f54/agentup-0.3.0.tar.gz"
+  sha256 "39c59031f8432bf8c795f58bdb4c18caa6ab11c046b90d6d2178fdc0fbde7d8a"
 
   depends_on "python@3.11"
 
